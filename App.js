6b2ac6ea32560ef8b7db199219148d9a6f970d3f
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2019-11-10 01:40:50 +0300

    basic home page designed

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the BikeSharing project!</Text>
+      <View style={styles.logo}>
+        <Image source={require("./images/bikelogo.png")} />
+      </View>
+      <View style={styles.login}>
+        <View>
+          <Text>Username:</Text>
+          <TextInput 
+            style={{height:30, width:200, backgroundColor:"#fff"}}
+          />
+          <Text>Password:</Text>
+          <TextInput
+            style={{height:30, width:200, backgroundColor:"#fff"}}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button title="Login"/>
+          <Button title="Sign Up"/>
+        </View>
+        <View>
+          <Text style={{color:"#222288"}}>Did you forget your password?</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +33,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#60ec80',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo:{
+    flex: 3,
+    justifyContent: 'flex-end',
+  },
+  login: {
+    flex: 2,
+    backgroundColor: '#60ec80',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  button:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    alignContent: 'center',
   },
 });
