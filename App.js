1b29849c298f3fdd4f4cdb119c901bde79febdd5
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2019-11-12 19:05:41 +0300

    sign up page created

--- App.js
+++ App.js
@@ -1,33 +1,75 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, View, Image, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <Image source={require("./images/bikelogo.png")} />
+class HomeScreen extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image source={require("./images/bikelogo.png")} />
+        </View> 
+        <View style={styles.login}>
+          <View>
+            <Text>Username:</Text>
+            <TextInput 
+              style={{height:30, width:200, backgroundColor:"#fff"}}
+            />
+            <Text>Password:</Text>
+            <TextInput
+              style={{height:30, width:200, backgroundColor:"#fff"}}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title="Login"/>
+            <Button title="Sign Up"
+              onPress={() => this.props.navigation.navigate('SignUp')}
+            />
+          </View>
+          <View>
+            <Text style={{color:"#222288"}}>Did you forget your password?</Text>
+          </View>
+        </View>
       </View>
-      <View style={styles.login}>
-        <View>
-          <Text>Username:</Text>
-          <TextInput 
-            style={{height:30, width:200, backgroundColor:"#fff"}}
-          />
-          <Text>Password:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff"}}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button title="Login"/>
-          <Button title="Sign Up"/>
-        </View>
-        <View>
-          <Text style={{color:"#222288"}}>Did you forget your password?</Text>
-        </View>
+    );
+  }
+}
+
+class SignUpScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Username:</Text>
+        <TextInput 
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+        />
+        <Text>Password:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+        />
+        <Button title="Sign Up"/>
       </View>
-    </View>
-  );
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    SignUp: SignUpScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
