b4f323fda6c699d3b1e918774658a40a5f5aa2ee
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2019-11-24 14:53:47 +0300

    HomeScreen page name changed to LoginScreen, code restored.

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,10 +1,10 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
-import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
 
 const AppNavigator = createStackNavigator({
-    Home: { screen: HomeScreen },
+    Login: { screen: LoginScreen },
     SignUp: { screen: SignUpScreen},
 });
 
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,112 +4,97 @@ import React from 'react';
 
 export default class SignUpScreen extends React.Component {
 
-    constructor()
-      {
-          super();
-   
-          this.state = {  username: "", 
-                          password: "",
-                          tcNo: "",
-                          name: "",
-                          surname: "",
-                          phone: "",
-                          eMail: "",
-                          birthDate: "",
-                          loading: false,
-                          disabled: false }
-          }
+  constructor(){
+        super();
+        this.state = {  username: "", 
+                        password: "",
+                        tcNo: "",
+                        name: "",
+                        surname: "",
+                        phone: "",
+                        eMail: "",
+                        birthDate: "",
+                        loading: false,
+                        disabled: false 
+                      }
+  }
   
-  
-  
-    saveData = () =>
-    {
-        this.setState({ loading: true, disabled: true }, () =>
-        {
-            fetch('http://144.122.192.199:3000/users/register',
-            {
-                method: 'POST',
-                headers: 
-                {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(
-                {
-                  
-                   tcNo : this.state.tcNo,
-                   name : this.state.name,
-                   surname : this.state.surname,
-                   phone : this.state.phone,
-                   eMail : this.state.eMail,
-                   userName : this.state.username,
-                   password : this.state.password,
-                   birthDate : this.state.birthDate
-                    
-                })
-  
-            }).then((response) => response.json()).then((responseJson) =>
-            {
-                alert(responseJson);
-                this.setState({ loading: false, disabled: false });
-            }).catch((error) =>
-            {
-                console.error(error);
-                this.setState({ loading: false, disabled: false });
-            });
-        });
-    }
-  
-  
-  
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Username:</Text>
-          <TextInput 
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ username: text })}
-          />
-          <Text>Password:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ password: text })}
-          />
-          <Text>T.C.:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ tcNo: text })}
-          />
-          <Text>Name:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ name: text })}
-          />
-          <Text>Surname:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ surname: text })}
-          />
-          <Text>Phone number:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ phone: text })}
-          />
-          <Text>E-mail:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ eMail: text })}
-          />
-          <Text>Birth date:</Text>
-          <TextInput
-            style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
-            onChangeText = {(text) => this.setState({ birthDate: text })}
-          />
-          <Button title="Sign Up"
-            onPress = { this.saveData }
-          />
-        </View>
-      );
-    }
+  saveData = () => {
+    this.setState({ loading: true, disabled: true }, () => {
+      fetch('http://144.122.192.199:3000/users/register', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            tcNo : this.state.tcNo,
+            name : this.state.name,
+            surname : this.state.surname,
+            phone : this.state.phone,
+            eMail : this.state.eMail,
+            userName : this.state.username,
+            password : this.state.password,
+            birthDate : this.state.birthDate
+        })
+      }).then((response) => response.json()).then((responseJson) => {
+            alert(responseJson);
+            this.setState({ loading: false, disabled: false });
+        }).catch((error) => {
+            console.error(error);
+            this.setState({ loading: false, disabled: false });
+          });
+    });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Username:</Text>
+        <TextInput 
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ username: text })}
+        />
+        <Text>Password:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ password: text })}
+        />
+        <Text>T.C.:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ tcNo: text })}
+        />
+        <Text>Name:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ name: text })}
+        />
+        <Text>Surname:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ surname: text })}
+        />
+        <Text>Phone number:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ phone: text })}
+        />
+        <Text>E-mail:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ eMail: text })}
+        />
+        <Text>Birth date:</Text>
+        <TextInput
+          style={{height:30, width:200, backgroundColor:"#fff", borderColor:"#000", borderWidth:2}}
+          onChangeText = {(text) => this.setState({ birthDate: text })}
+        />
+        <Button title="Sign Up"
+          onPress = { this.saveData }
+        />
+      </View>
+    );
+  }
 }
   
