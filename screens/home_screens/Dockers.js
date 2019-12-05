bdc2f577f0fb0d53bb557a371d8f7e75623eca17
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2019-12-06 02:17:34 +0300

    Map added to dockers page.

--- screens/home_screens/Dockers.js
+++ screens/home_screens/Dockers.js
@@ -1,8 +1,34 @@
-import {Text} from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import React from 'react';
+import MapView from 'react-native-maps';
+
 
 export default class Dockers extends React.Component{
-    render () {
-        return(<Text>Dockers</Text>);
+    render() {
+        return (
+          <View style={styles.container}>
+            <MapView 
+                style={styles.mapStyle}
+                initialRegion={{
+                    latitude: 39.897053,
+                    longitude: 32.778302,
+                    latitudeDelta: 0.0422,
+                    longitudeDelta: 0,
+                  }} />
+          </View>
+        );
     }
 }
+    
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+    mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    },
+});
