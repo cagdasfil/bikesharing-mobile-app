fe7029a2a78954591168daa3476d067060816247
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2019-12-07 03:35:57 +0300

    Markers are added to the Dockers page.

--- screens/home_screens/Dockers.js
+++ screens/home_screens/Dockers.js
@@ -1,6 +1,6 @@
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker, MapViewAnimated } from 'react-native-maps';
 
 
 export default class Dockers extends React.Component{
@@ -12,23 +12,51 @@ export default class Dockers extends React.Component{
                 initialRegion={{
                     latitude: 39.897053,
                     longitude: 32.778302,
-                    latitudeDelta: 0.0422,
+                    latitudeDelta: 0.0522,
                     longitudeDelta: 0,
-                  }} />
+                  }}>
+              <Marker
+                coordinate={{latitude:39.907216, longitude: 32.783874,}}
+                title="12 bikes are available"
+              >
+                <View style={styles.marker}>
+                  <Text style={{color:'white', fontWeight:'bold', fontSize:18}}>12</Text>
+                </View>
+              </Marker>
+              <Marker
+                coordinate={{latitude:39.891434, longitude: 32.793641,}}
+                title="17 bikes are available"
+              >
+                <View style={styles.marker}>
+                  <Text style={{color:'white', fontWeight:'bold', fontSize:18}}>17</Text>
+                </View>
+              </Marker>
+            </MapView>
           </View>
         );
     }
 }
     
 const styles = StyleSheet.create({
-    container: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    },
-    mapStyle: {
+  },
+  mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-    },
+  },
+  marker: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'green',
+    borderColor:'white', 
+    borderWidth:1,
+    width:40,
+    height:40,
+    borderRadius:20
+  }
 });
