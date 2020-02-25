0b7b5138f85fd5964323a5b94fa6cc050f1ff5a0
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-02-25 19:38:17 +0300

    Report page improved.

--- screens/Report.js
+++ screens/Report.js
@@ -13,17 +13,19 @@ export default class Report extends React.Component{
                     <Ionicons name="md-menu" color={theme.COLORS.SEASHELL} size={35}/>
                 </TouchableOpacity>
                 <View style={{flex:1, justifyContent:'center', margin:20}}>
-                    <Text style={{marginLeft:5, marginBottom:5}}>Title :</Text>
-                    <TextInput style={{borderWidth:1, marginBottom:5, paddingLeft:5}}/>
-                    <Text style={{marginLeft:5, marginBottom:5}}>Description :</Text>
-                    <TextInput style={{borderWidth:1, marginBottom:5, paddingLeft:5}} numberOfLines={8} textAlignVertical={"top"}/>
+                    <Text style={{marginLeft:5, marginBottom:5, color:theme.COLORS.JAPANESE_INDIGO}}>Title :</Text>
+                    <TextInput style={{borderWidth:1, borderRadius:3, borderColor:theme.COLORS.JAPANESE_INDIGO, backgroundColor:"white", marginBottom:5, paddingLeft:5}}/>
+                    <Text style={{marginLeft:5, marginBottom:5, color:theme.COLORS.JAPANESE_INDIGO}}>Description :</Text>
+                    <TextInput style={{borderWidth:1, borderRadius:3, borderColor:theme.COLORS.JAPANESE_INDIGO, backgroundColor:"white", marginBottom:5, paddingLeft:5}} numberOfLines={8} textAlignVertical={"top"}/>
                     <View style={{flexDirection:'row', alignItems:'center', marginLeft:5}}>
-                        <Text>Add an image : </Text>
-                        <Ionicons name="md-camera" style={{}} size={30}/>
+                        <Text style={{color:theme.COLORS.JAPANESE_INDIGO}}>Add an image : </Text>
+                        <TouchableOpacity>
+                            <Ionicons name="md-camera" color={theme.COLORS.JAPANESE_INDIGO} size={30}/>
+                        </TouchableOpacity>
                     </View>
-                    <TouchableOpacity style={{borderWidth:1, marginVertical:30, marginHorizontal:60,
+                    <TouchableOpacity style={{marginVertical:40, marginHorizontal:60, backgroundColor:theme.COLORS.JAPANESE_INDIGO,
                             flexDirection:"row", alignItems:'center', justifyContent:'center', height:40}}>
-                        <Text style={{fontWeight:'bold', fontSize:16}}>SEND</Text>
+                        <Text style={{fontWeight:'bold', fontSize:16, color:"white"}}>SEND</Text>
                     </TouchableOpacity>
                 </View>
             </View>
