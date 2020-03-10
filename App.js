ee2887c624ebd825cb3789d4730487e9c3e35bb0
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-03-10 19:49:47 +0300

    notification created.

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { Constants, Permissions, Notifications } from 'expo';
 import Screen from './navigation/Screen';
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('reminders', {
+        name: 'Reminders',
+        priority: 'max',
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  }
+  
   render() {
     return <Screen />;
   }
--- screens/Notifications.js
+++ screens/Notifications.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import theme from '../constants/Theme';
+import { Constants, Permissions, Notifications } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
-export default class Notifications extends React.Component{
+export default class NotificationsPage extends React.Component{
+
+    _createNotificationAsync = () => {
+        Notifications.presentLocalNotificationAsync({
+          title: 'New Virtual Zone',
+          body: 'There is a new virtual zone in your area. Don\'t forget to check out !',
+          android: {
+            channelId: 'reminders',
+            color: '#23395B',
+            icon: "https://img.icons8.com/cotton/64/000000/bicycle--v2.png",
+          },
+        });
+      }
+
+    componentWillMount(){
+        this._createNotificationAsync();
+    }
+
     render(){
         return(
             <View style={{flex:1, backgroundColor:theme.COLORS.SEASHELL}}>
